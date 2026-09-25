d7905623186444fa500daae76367c95db49feb9b
sy220284 <2648897872@qq.com>
2026-09-25 11:53:13 +0800

    补齐表格横向滚动依赖

--- app/src/main/java/com/labteto/dshmobile/ui/components/Markdown.kt
+++ app/src/main/java/com/labteto/dshmobile/ui/components/Markdown.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
