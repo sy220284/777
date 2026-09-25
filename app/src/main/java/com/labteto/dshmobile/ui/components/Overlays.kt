0aafce3cf320f6442d6d31096e86351e251f083d
sy220284 <2648897872@qq.com>
2026-09-25 12:22:44 +0800

    chore(ui): remove unused color import

--- app/src/main/java/com/labteto/dshmobile/ui/components/Overlays.kt
+++ app/src/main/java/com/labteto/dshmobile/ui/components/Overlays.kt
@@ -36,7 +36,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
